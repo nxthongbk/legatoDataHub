60dce0b078190d89c3bc9f3e38b3b07ded17b938
Jen Chitty <jchitty@sierrawireless.com>
2018-04-17 13:47:57 -0700

    Rename actuator counter to counter/value

--- test/actuator/actuator.c
+++ test/actuator/actuator.c
@@ -1,6 +1,8 @@
 #include "legato.h"
 #include "interfaces.h"
 
+#define COUNTER_NAME "counter/value"
+
 //--------------------------------------------------------------------------------------------------
 /**
  * Call-back function called when an update is received from the Data Hub for the "counter" value.
@@ -39,27 +41,27 @@ static void NumericCounterUpdateHandler
         LE_INFO("Running create/delete tests");
 
         // Create the Output with different type and units and expect LE_DUPLICATE.
-        le_result_t result = io_CreateOutput("counter", IO_DATA_TYPE_STRING, "count");
+        le_result_t result = io_CreateOutput(COUNTER_NAME, IO_DATA_TYPE_STRING, "count");
         LE_ASSERT(result == LE_DUPLICATE);
-        result = io_CreateOutput("counter", IO_DATA_TYPE_NUMERIC, "s");
+        result = io_CreateOutput(COUNTER_NAME, IO_DATA_TYPE_NUMERIC, "s");
         LE_ASSERT(result == LE_DUPLICATE);
-        result = io_CreateInput("counter", IO_DATA_TYPE_NUMERIC, "count");
+        result = io_CreateInput(COUNTER_NAME, IO_DATA_TYPE_NUMERIC, "count");
         LE_ASSERT(result == LE_DUPLICATE);
 
         // Create the Output with same type and units and expect LE_OK.
-        result = io_CreateOutput("counter", IO_DATA_TYPE_NUMERIC, "count");
+        result = io_CreateOutput(COUNTER_NAME, IO_DATA_TYPE_NUMERIC, "count");
         LE_ASSERT(result == LE_OK);
 
         // Delete the Input and re-create it.
-        io_DeleteResource("counter");
-        result = io_CreateOutput("counter", IO_DATA_TYPE_NUMERIC, "count");
+        io_DeleteResource(COUNTER_NAME);
+        result = io_CreateOutput(COUNTER_NAME, IO_DATA_TYPE_NUMERIC, "count");
         LE_ASSERT(result == LE_OK);
-        io_AddNumericPushHandler("counter", NumericCounterUpdateHandler, NULL);
-        io_JsonPushHandlerRef_t jsonHandlerRef = io_AddJsonPushHandler("counter",
+        io_AddNumericPushHandler(COUNTER_NAME, NumericCounterUpdateHandler, NULL);
+        io_JsonPushHandlerRef_t jsonHandlerRef = io_AddJsonPushHandler(COUNTER_NAME,
                                                                        JsonCounterUpdateHandler,
                                                                        NULL);
         io_RemoveJsonPushHandler(jsonHandlerRef);
-        io_AddJsonPushHandler("counter", JsonCounterUpdateHandler, NULL);
+        io_AddJsonPushHandler(COUNTER_NAME, JsonCounterUpdateHandler, NULL);
     }
 }
 
@@ -69,11 +71,11 @@ COMPONENT_INIT
     le_result_t result;
 
     // This will be received from the Data Hub.
-    result = io_CreateOutput("counter", IO_DATA_TYPE_NUMERIC, "count");
+    result = io_CreateOutput(COUNTER_NAME, IO_DATA_TYPE_NUMERIC, "count");
     LE_ASSERT(result == LE_OK);
 
     // Register for notification of updates to the counter value.
-    io_AddNumericPushHandler("counter", NumericCounterUpdateHandler, NULL);
-    io_AddJsonPushHandler("counter", JsonCounterUpdateHandler, NULL);
-    io_AddStringPushHandler("counter", JsonCounterUpdateHandler, NULL);
+    io_AddNumericPushHandler(COUNTER_NAME, NumericCounterUpdateHandler, NULL);
+    io_AddJsonPushHandler(COUNTER_NAME, JsonCounterUpdateHandler, NULL);
+    io_AddStringPushHandler(COUNTER_NAME, JsonCounterUpdateHandler, NULL);
 }
